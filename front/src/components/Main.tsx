c6521f109ab6a5e96a51f07edeb1361c311af1e2
Nami <havenothorn@gmail.com>
2023-12-05 16:54:02 +0900

    style: 불필요한 fragment 제거

--- front/src/components/Main.tsx
+++ front/src/components/Main.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Main = (props: { children: React.ReactNode }) => {
-  return (
-    <>
-      <main className="main">{props.children}</main>
-    </>
-  );
+  return <main className="main">{props.children}</main>;
 };
 
 export default Main;
